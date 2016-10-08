cab77a3d600b624ec2915cdc8b4a8e66d0199c93
neil patel <neil@myindianweddingapp.com>
2016-10-08 11:16:14 +0100

    Implemented new index query for showing users posts they created

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -19,6 +19,18 @@ describe 'navigate' do
       expect(page).to have_content(/Posts/)
     end
 
+    it 'has a scope so that only post creators can see their posts' do
+      post1 = Post.create(date: Date.today, rationale: "asdsf", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "asdsf", user_id: @user.id)
+
+      other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      post_from_other_user =  Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
+
+      visit posts_path
+      expect(page).to_not have_content(/This post shouldn't be seen/)
+    end
+  end
+
     it 'has a list of posts' do
       post1 = FactoryGirl.build_stubbed(:post)
       post2 = FactoryGirl.build_stubbed(:second_post)
@@ -39,6 +51,7 @@ describe 'navigate' do
   describe 'delete' do
     it 'can be deleted' do
       @post = FactoryGirl.create(:post)
+      @post.update(user_id: @user.id)
       visit posts_path
 
       click_link("delete_post_#{@post.id}_from_index")
