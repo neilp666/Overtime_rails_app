31fdaa145b050146dca7f7b1cca44c954b219acd
neil patel <neil@myindianweddingapp.com>
2016-10-16 18:40:32 +0100

    Implemented audit log

---
+++ spec/factories/audit_logs.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :audit_log do
+    user
+    status 0
+    start_date (Date.today - 6.days)
+    end_date nil
+  end
+end
+
+AuditLog(user_id: User.last.id, status: 0, start_date (Date.today - 6.days))
