4a082df6806d1dc5fee2f68988fd7a26c66d7c3f
neil patel <neil@myindianweddingapp.com>
2016-10-25 21:54:47 +0100

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-@user = User.create(email: "test@test.com", 
-                    password: "asdfasdf", 
-                    password_confirmation: "asdfasdf", 
-                    first_name: "Jon", 
-                    last_name: "Snow"),
+@user = User.create(email: "test@test.com",
+                    password: "asdfasdf",
+                    password_confirmation: "asdfasdf",
+                    first_name: "Jon",
+                    last_name: "Snow",
                     phone: "01772123456")
 
 puts "1 User created"
 
-@user = AdminUser.create(email: "admin@test.com", 
+AdminUser.create(email: "admin@test.com", 
                          password: "asdfasdf", 
                          password_confirmation: "asdfasdf", 
                          first_name: "Jon", 
-                         last_name: "Snow"),
+                         last_name: "Snow",
                          phone: "01772123456")
 
 puts "1 Admin User created"
@@ -23,7 +23,7 @@ end
 puts "100 Posts have been created"
 
 100.times do |audit_log|
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
 puts "100 audit logs have been created"
